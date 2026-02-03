648eccb654d5dd10ae4581a6c4a3c08b6c4fd48d
Ashwini Bhat M <asbhat821@gmail.com>
2026-02-03 20:30:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import Header from './Header';
+import Footer from './Footer';
+import Login from './Login';
+ 
+ 
+function App() {
+ 
+  return (
+    <>
+      <Header/>
+      <Login/>
+      <Footer/>
+    </>
+  );
+}
+ 
+export default App;
